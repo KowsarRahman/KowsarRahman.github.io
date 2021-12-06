3f602e0d95adb8c7aca0d23aefaa4b354454bcc7
KowsarRahman <s3825159@student.rmit.edu.au>
2021-12-06 17:55:16 +0600

    Build file added

--- pages/components/Footer.js
+++ pages/components/Footer.js
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import React, { Component } from "react";
 
 class Footer extends Component {
@@ -11,9 +12,9 @@ class Footer extends Component {
       <footer className='py-3 my-4'>
         <ul className='nav justify-content-center border-bottom pb-3 mb-3'>
           <li className='nav-item'>
-            <a href='/' className='nav-link px-2 text-muted'>
+            <Link href='/' className='nav-link px-2 text-muted'>
               Home
-            </a>
+            </Link>
           </li>
         </ul>
         <p className='text-center text-muted'>© 2021 Kowsar Rahman, Inc</p>
--- pages/components/NavBar.js
+++ pages/components/NavBar.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Avatar } from "@mui/material";
+import Link from 'next/link'
 import { deepOrange, deepPurple } from '@mui/material/colors';
 
 class NavBar extends Component {
@@ -10,13 +11,13 @@ class NavBar extends Component {
         return(
         <>
         <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-        <Avatar sx={{ bgcolor: deepPurple[500] }}>KR</Avatar>&nbsp;<a className="navbar-brand" href="/">Kowsar Rahman</a>
+        <Avatar sx={{ bgcolor: deepPurple[500] }}>KR</Avatar>&nbsp;<Link className="navbar-brand" href="/">Kowsar Rahman</Link>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
         <div className="navbar-nav">
-        <a className="nav-item nav-link active" href="#">Home</a>
+        <Link className="nav-item nav-link active" href="#">Home</Link>
         </div>
         </div>
         </nav>
